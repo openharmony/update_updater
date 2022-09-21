46eae0de3ca3b097cb8bd4459fa215c22b909221
lidanyang <lidanyang12@huawei.com>
2022-09-21 12:20:39 +0800

    add test case for json
    Signed-off-by: lidanyang <lidanyang12@huawei.com>

--- test/unittest/utils/json/utils_json_visitor_test.cpp
+++ test/unittest/utils/json/utils_json_visitor_test.cpp
@@ -75,6 +75,22 @@ using namespace Updater;
 using namespace std;
 using namespace testing::ext;
 
+namespace {
+using PairType = std::pair<std::string_view, std::string_view>;
+template<typename T, std::size_t N>
+void TestInvalidCases(T &obj, const std::array<PairType, N> &replaceData, std::string &jsonStr)
+{
+    for (auto data : replaceData) {
+        auto pos = jsonStr.find(data.first);
+        ASSERT_NE(pos, std::string::npos) << data.first;
+        jsonStr.replace(pos, data.first.size(), data.second.data(), data.second.size());
+        JsonNode node {jsonStr};
+        EXPECT_EQ(false, Visit<SETVAL>(node[Traits<T>::STRUCT_KEY], obj)) << data.first;
+        jsonStr.replace(jsonStr.find(data.second), data.second.size(), data.first.data(), data.first.size());
+    }
+}
+}
+
 namespace UpdaterUt {
 // do something at the each function begining
 void UtilsJsonVisitorUnitTest::SetUp(void)
@@ -100,176 +116,213 @@ void UtilsJsonVisitorUnitTest::TearDownTestCase(void)
     cout << "TearDownTestCase" << endl;
 }
 
-std::string g_jsonStr = R"({
-    "D": {
-        "d1":1,
-        "d2":"true",
-        "d3":true
-    },
-    "E": {
-        "d1":1,
-        "d2": {
-            "r":1,
-            "g":"foo",
-            "b":true
-        }
-    },
-    "H": {
-        "d1":1,
-        "d2":true,
-        "d3": {
-            "d1":2,
-            "d2":true,
-            "d3": {
-                "d1":3,
-                "d2":"foo"
-            }
-        }
-    },
-    "I" : {
-        "d1": [
-            "foo",
-            "bar",
-            "baz"
-        ]
-    },
-    "J" : {
-        "d1" : [
-            [
-                "foo",
-                "bar",
-                "baz"
-            ],
-            [
-                "foo1",
-                "bar1",
-                "baz1"
-            ]
-        ]
-    },
-    "K" : {
-        "d1" : 1
-    },
-    "KNonDefault0" : {
-        "d1" : 2
-    },
-    "KNonDefault1" : {
-        "d1" : 2,
-        "d2" : "v2"
-    },
-    "L" : {
-        "d1" : [1]
-    },
-    "LNonDefault0" : {
-        "d1" : [2]
-    },
-    "LNonDefault1" : {
-        "d1" : "2"
-    },
-    "LNonDefault2" : {
-        "d1" : ["2"]
-    }
-})";
-
-JsonNode g_node(g_jsonStr);
-
 HWTEST_F(UtilsJsonVisitorUnitTest, testD, TestSize.Level0)
 {
+    std::string dJson = R"({
+        "D": {
+            "d1":1,
+            "d2":"true",
+            "d3":true
+        }
+    })";
+    JsonNode node {dJson};
     D d {};
-    bool res = Visit<SETVAL>(g_node["D"], d);
-    EXPECT_EQ(res, true);
+    EXPECT_EQ(Visit<SETVAL>(node["D"], d), true);
     EXPECT_EQ(d.d1, 1);
     EXPECT_EQ(d.d2, "true");
     EXPECT_EQ(d.d3, true);
+
+    constexpr std::array replaceData {
+        PairType { R"("d1":1)", R"("d1":"1")" }, PairType { R"("d2":"true")", R"("d2":true)" },
+        PairType { R"("d3":true)", R"("d3":"true")" }
+    };
+    TestInvalidCases(d, replaceData, dJson);
 }
 
 HWTEST_F(UtilsJsonVisitorUnitTest, testE, TestSize.Level0)
 {
+    std::string eJson = R"({
+        "E": {
+            "d1":1,
+            "d2": {
+                "r":1,
+                "g":"foo",
+                "b":true
+            }
+        }
+    })";
     E e {};
-    bool res = Visit<SETVAL>(g_node["E"], e);
-    EXPECT_EQ(res, true);
+    JsonNode node {eJson};
+    EXPECT_EQ(Visit<SETVAL>(node["E"], e), true);
     EXPECT_EQ(e.d1, 1);
     EXPECT_EQ(e.d2.r, 1);
     EXPECT_EQ(e.d2.g, "foo");
     EXPECT_EQ(e.d2.b, true);
+
+    constexpr std::array replaceData {
+        PairType { R"("d1":1)", R"("d1":"1")" }, PairType { R"("r":1)", R"("r":"1")" },
+        PairType { R"("g":"foo")", R"("g":1)" }, PairType { R"("b":true)", R"("b":"true")" }
+    };
+    TestInvalidCases(e, replaceData, eJson);
 }
 
 HWTEST_F(UtilsJsonVisitorUnitTest, testH, TestSize.Level0)
 {
+    std::string hJson = R"({
+        "H": {
+            "d1":1,
+            "d2":true,
+            "d3": {
+                "d1":2,
+                "d2":false,
+                "d3": {
+                    "d1":3,
+                    "d2":"foo"
+                }
+            }
+        }
+    })";
     H h {};
-    bool res = Visit<SETVAL>(g_node["H"], h);
-    EXPECT_EQ(res, true);
+    JsonNode node {hJson};
+    EXPECT_EQ(Visit<SETVAL>(node["H"], h), true);
     EXPECT_EQ(h.d1, 1);
     EXPECT_EQ(h.d2, true);
     EXPECT_EQ(h.d3.d1, 2);
-    EXPECT_EQ(h.d3.d2, true);
+    EXPECT_EQ(h.d3.d2, false);
     EXPECT_EQ(h.d3.d3.d1, 3);
     EXPECT_EQ(h.d3.d3.d2, "foo");
+
+    constexpr std::array replaceData {
+        PairType { R"("d1":1)", R"("d1":"1")" }, PairType { R"("d1":2)", R"("d1":"2")" },
+        PairType { R"("d1":3)", R"("d1":"3")" }, PairType { R"("d2":true)", R"("d2":"true")" },
+        PairType { R"("d2":false)", R"("d2":"false")" }, PairType { R"("d2":"foo")", R"("d2":1)" },
+        PairType { R"("d3": {
+                "d1":2,
+                "d2":false,
+                "d3": {
+                    "d1":3,
+                    "d2":"foo"
+                }
+            })", R"("d3":1)"},
+        PairType { R"("d3": {
+                    "d1":3,
+                    "d2":"foo"
+                })", R"("d3":2)"}
+    };
+    TestInvalidCases(h, replaceData, hJson);
 }
 
 HWTEST_F(UtilsJsonVisitorUnitTest, testI, TestSize.Level0)
 {
+    std::string iJson = R"({
+        "I" : {
+            "d1": [
+                "foo",
+                "bar",
+                "baz"
+            ]
+        }
+    })";
     I i {};
-    bool res = Visit<SETVAL>(g_node["I"], i);
-    EXPECT_EQ(res, true);
+    JsonNode node {iJson};
+    EXPECT_EQ(Visit<SETVAL>(node["I"], i), true);
     EXPECT_EQ(i.d1, std::vector<std::string>({"foo", "bar", "baz"}));
+    EXPECT_EQ(Visit<SETVAL>(JsonNode {R"({"I" : { "d1" : 1 }})"s} ["I"], i), false);
 }
 
 HWTEST_F(UtilsJsonVisitorUnitTest, testJ, TestSize.Level0)
 {
+    std::string jJson = R"({
+        "J" : {
+            "d1" : [
+                [
+                    "foo",
+                    "bar",
+                    "baz"
+                ],
+                [
+                    "foo1",
+                    "bar1",
+                    "baz1"
+                ]
+            ]
+        }
+    })";
     J j {};
-    bool res = Visit<SETVAL>(g_node["J"], j);
-    EXPECT_EQ(res, true);
+    JsonNode node {jJson};
+    EXPECT_EQ(Visit<SETVAL>(node["J"], j), true);
     ASSERT_EQ(j.d1.size(), 2);
     EXPECT_EQ(j.d1[0], std::vector<std::string>({"foo", "bar", "baz"}));
     EXPECT_EQ(j.d1[1], std::vector<std::string>({"foo1", "bar1", "baz1"}));
+    EXPECT_EQ(Visit<SETVAL>(JsonNode {R"({"J" : { "d1" : 1 }})"s} ["J"], j), false);
+    EXPECT_EQ(Visit<SETVAL>(JsonNode {R"({"J" : { "d1" : [1] }})"s} ["J"], j), false);
+    EXPECT_EQ(Visit<SETVAL>(JsonNode {R"({"J" : { "d1" : [[1]] }})"s} ["J"], j), false);
 }
 
 HWTEST_F(UtilsJsonVisitorUnitTest, testInvalidK, TestSize.Level0)
 {
+    std::string kJson = R"({
+        "K" : {
+            "d1" : 1
+        }
+    })";
     K k {};
-    bool res = Visit<SETVAL>(g_node["K"], k);
-    EXPECT_EQ(res, false);
+    JsonNode node {kJson};
+    EXPECT_EQ(Visit<SETVAL>(node["K"], k), false);
 }
 
 HWTEST_F(UtilsJsonVisitorUnitTest, testNoDefaultAndNonDefaultK, TestSize.Level0)
 {
+    std::string kJson = R"({
+        "K" : {
+            "d1" : 1
+        },
+        "KNonDefault0" : {
+            "d1" : 2
+        },
+        "KNonDefault1" : {
+            "d1" : 2,
+            "d2" : "v2"
+        }
+    })";
     K k {};
-    bool res = Visit<SETVAL>(g_node["KNonDefault0"], g_node["K"], k);
-    EXPECT_EQ(res, false);
-
-    res = Visit<SETVAL>(g_node["KNonDefault1"], g_node["K"], k);
-    EXPECT_EQ(res, true);
+    JsonNode node {kJson};
+    EXPECT_EQ(Visit<SETVAL>(node["KNonDefault0"], node["K"], k), false);
+    EXPECT_EQ(Visit<SETVAL>(node["KNonDefault0"], JsonNode {"{"s}, k), false);
+    EXPECT_EQ(Visit<SETVAL>(JsonNode {"{"s}, node["KNonDefault0"], k), false);
+    EXPECT_EQ(Visit<SETVAL>(node["KNonDefault1"], node["K"], k), true);
     EXPECT_EQ(k.d1, 2);
     EXPECT_EQ(k.d2, "v2");
 }
 
 HWTEST_F(UtilsJsonVisitorUnitTest, testArrayL, TestSize.Level0)
 {
+    std::string lJson = R"({
+        "L" : {
+            "d1" : [1]
+        },
+        "LNonDefault0" : {
+            "d1" : [2]
+        },
+        "LNonDefault1" : {
+            "d1" : "2"
+        },
+        "LNonDefault2" : {
+            "d1" : ["2"]
+        }
+    })";
     L l {};
-    bool res = Visit<SETVAL>(g_node["LNonDefault0"], g_node["L"], l);
-    EXPECT_EQ(res, true);
+    JsonNode node {lJson};
+    EXPECT_EQ(Visit<SETVAL>(node["LNonDefault0"], node["L"], l), true);
     EXPECT_EQ(l.d1, std::vector<int>({2, 1}));
 
     l = {};
-    res = Visit<SETVAL>(g_node["L"], g_node["LNonDefault0"], l);
-    EXPECT_EQ(res, true);
+    EXPECT_EQ(Visit<SETVAL>(node["L"], node["LNonDefault0"], l), true);
     EXPECT_EQ(l.d1, std::vector<int>({1, 2}));
 
-    l = {};
-    res = Visit<SETVAL>(g_node["LNonDefault1"], g_node["L"], l);
-    EXPECT_EQ(res, false);
-
-    l = {};
-    res = Visit<SETVAL>(g_node["L"], g_node["LNonDefault1"], l);
-    EXPECT_EQ(res, false);
-
-    l = {};
-    res = Visit<SETVAL>(g_node["L"], g_node["LNonDefault2"], l);
-    EXPECT_EQ(res, false);
-
-    l = {};
-    res = Visit<SETVAL>(g_node["LNonDefault2"], g_node["L"], l);
-    EXPECT_EQ(res, false);
+    EXPECT_EQ(Visit<SETVAL>(node["LNonDefault1"], node["L"], l), false);
+    EXPECT_EQ(Visit<SETVAL>(node["L"], node["LNonDefault1"], l), false);
+    EXPECT_EQ(Visit<SETVAL>(node["L"], node["LNonDefault2"], l), false);
+    EXPECT_EQ(Visit<SETVAL>(node["LNonDefault2"], node["L"], l), false);
 }
 } // namespace UpdaterUt
