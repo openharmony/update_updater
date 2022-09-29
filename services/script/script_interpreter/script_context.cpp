5ac461e4127cf8a343bc0424de71a9a03b92b49c
hughes802 <h.hu@huawei.com>
2022-09-29 14:17:12 +0800

    Signed-off-by: hughes802 <h.hu@huawei.com>

--- services/script/script_interpreter/script_context.cpp
+++ services/script/script_interpreter/script_context.cpp
@@ -77,14 +77,13 @@ int32_t UScriptInstructionContext::GetParam(int32_t index, T &value)
         LOG(ERROR) << "Invalid index " << index;
         return UScriptContext::PARAM_TYPE_INVALID;
     }
-    // check whether TWapper's type is same with inner param's type
-    TWapper v {T {}};
-    if (innerParam_[index].get()->GetValueType() != v.GetValueType()) {
+
+    // then perform cast between innerparam and TWapper
+    TWapper* inter = static_cast<TWapper*>(innerParam_[index].get());
+    if (inter == nullptr) {
         LOG(ERROR) << "Invalid index " << index;
         return USCRIPT_INVALID_PARAM;
     }
-    // then perform cast between innerparam and TWapper
-    TWapper* inter = static_cast<TWapper*>(innerParam_[index].get());
     value = inter->GetValue();
     return USCRIPT_SUCCESS;
 }
