7e6d9a73c20ca71c1abfd9fd60fa1d7684831e62
chenzihan <chenzihan13@huawei.com>
2022-10-26 11:09:08 +0800

    Signed-off-by: chenzihan <chenzihan13@huawei.com>
    fix:fix about package_unittest

--- interfaces/kits/packages/package.cpp
+++ interfaces/kits/packages/package.cpp
@@ -231,19 +231,18 @@ int32_t CreatePackageL1(const UpgradePkgInfoExt *pkgInfo, ComponentInfoExt *comp
     return ret;
 }
 
-int32_t ExtraPackageDir(const char *packagePath, const char *keyPath, const char *dir,
-    const char *outPath)
+int32_t ExtraPackageDir(const char *packagePath, const char *dir, const char *outPath)
 {
     PkgManager::PkgManagerPtr manager = PkgManager::GetPackageInstance();
-    if (packagePath == nullptr || keyPath == nullptr || outPath == nullptr || manager == nullptr) {
+    if (packagePath == nullptr || outPath == nullptr || manager == nullptr) {
         LOG(ERROR) << "Check param fail ";
         return PKG_INVALID_PARAM;
     }
 
     std::vector<std::string> components;
-    int32_t ret = manager->LoadPackage(std::string(packagePath), std::string(keyPath), components);
+    int32_t ret = manager->LoadPackageWithoutUnPack(std::string(packagePath), components);
     if (ret != PKG_SUCCESS) {
-        LOG(ERROR) << "LoadPackage fail";
+        LOG(ERROR) << "LoadPackageWithoutUnPack fail";
         PkgManager::ReleasePackageInstance(manager);
         return ret;
     }
@@ -266,19 +265,18 @@ int32_t ExtraPackageDir(const char *packagePath, const char *keyPath, const char
     return PKG_SUCCESS;
 }
 
-int32_t ExtraPackageFile(const char *packagePath, const char *keyPath, const char *file,
-    const char *outPath)
+int32_t ExtraPackageFile(const char *packagePath, const char *file, const char *outPath)
 {
     PkgManager::PkgManagerPtr manager = PkgManager::GetPackageInstance();
-    if (packagePath == nullptr || keyPath == nullptr || outPath == nullptr || manager == nullptr) {
+    if (packagePath == nullptr || outPath == nullptr || file == nullptr || manager == nullptr) {
         LOG(ERROR) << "Check param fail ";
         return PKG_INVALID_PARAM;
     }
 
     std::vector<std::string> components;
-    int32_t ret = manager->LoadPackage(std::string(packagePath), std::string(keyPath), components);
+    int32_t ret = manager->LoadPackageWithoutUnPack(std::string(packagePath), components);
     if (ret != PKG_SUCCESS) {
-        LOG(ERROR) << "LoadPackage fail";
+        LOG(ERROR) << "LoadPackageWithoutUnPack fail";
         PkgManager::ReleasePackageInstance(manager);
         return ret;
     }
