1c6a1926a14d617e729fbc4fa52809291e549f19
qy136 <18772452388@163.com>
2023-01-16 11:50:36 +0800

    add
    Signed-off-by: qy136 <18772452388@163.com>

--- services/log/log.cpp
+++ services/log/log.cpp
@@ -52,7 +52,7 @@ UpdaterLogger::~UpdaterLogger()
     }
 #ifndef DIFF_PATCH_SDK
     static const LogLevel LOG_LEVEL[] = { LOG_DEBUG, LOG_INFO, LOG_WARN, LOG_ERROR, LOG_FATAL };
-    HiLogBasePrint(LOG_CORE, LOG_LEVEL[level_], g_domain, g_logTag, "%{public}s", str.c_str());
+    HiLogBasePrint(LOG_CORE, LOG_LEVEL[level_], g_domain, g_logTag.c_str(), "%{public}s", str.c_str());
 #endif
     oss_.str("");
     oss_ << std::endl << std::flush;
