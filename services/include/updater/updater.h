f32e5381d56573cfb7f8b3d2fd794743196e9162
lidanyang <lidanyang12@huawei.com>
2023-05-13 21:54:55 +0800

    refactor SetMessageToMisc, to distinguish different mode when power failure rebooting
    Signed-off-by: lidanyang <lidanyang12@huawei.com>

--- services/include/updater/updater.h
+++ services/include/updater/updater.h
@@ -49,6 +49,7 @@ struct UpdaterParams {
     float initialProgress = 0; /* The upgrade starts at the progress bar location */
     float currentPercentage = 0; /* The proportion of progress bars occupied by the upgrade process */
     unsigned int pkgLocation = 0;
+    std::string miscCmd {"boot_updater"};
     std::vector<std::string> updatePackage {};
     std::function<void(float)> callbackProgress {};
 };
