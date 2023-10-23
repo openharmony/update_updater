76070b3654345dcc5a5bb2a5b63f09767138050a
wangjiadong <wangjiadong14@huawei.com>
2023-10-23 21:35:01 +0800

    Signed-off-by: wangjiadong <wangjiadong14@huawei.com>
    fix

--- test/fuzztest/updatermain_fuzzer/updatermain_fuzzer.h
+++ test/fuzztest/updatermain_fuzzer/updatermain_fuzzer.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,9 +13,9 @@
  * limitations under the License.
  */
 
-#ifndef DO_PARTTIONS_FUZZER_H
-#define DO_PARTTIONS_FUZZER_H
+#ifndef UPDATER_MAIN_FUZZER_H
+#define UPDATER_MAIN_FUZZER_H
 
-#define FUZZ_PROJECT_NAME "dopartitions_fuzzer"
+#define FUZZ_PROJECT_NAME "updatermain_fuzzer"
 
-#endif /* DO_PARTTIONS_FUZZER_H */
+#endif /* UPDATER_MAIN_FUZZER_H */
