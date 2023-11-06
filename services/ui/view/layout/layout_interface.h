e4d89c7bb8b338be13f79e4192d02f7232b4e921
Rayllll <leizhonghang@huawei.com>
2023-11-06 15:24:34 +0800

    Signed-off-by: Rayllll <leizhonghang@huawei.com>
    add auto layout

---
+++ services/ui/view/layout/layout_interface.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef LAYOUT_INTERFACE_H
+#define LAYOUT_INTERFACE_H
+#include "json_visitor.h"
+namespace Updater {
+class LayoutInterface {
+public:
+    virtual void Init() = 0;
+    virtual void InitParams(int width, int height, const std::string &deviceType, const std::string &ruleFilePath) = 0;
+    virtual void SetJsonLocation(JsonNode &root) = 0;
+    virtual ~LayoutInterface() {}
+};
+}
+#endif
