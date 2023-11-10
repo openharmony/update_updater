fcdf2681ef4e65db85703ee59a79729d9fda0ed6
lizhao <lizhao104@huawei.com>
2023-11-10 11:55:47 +0800

    The progress thread sets the progress bar uniformly
    Signed-off-by: lizhao <lizhao104@huawei.com>

--- services/flow_update/update_bin/component_processor.h
+++ services/flow_update/update_bin/component_processor.h
@@ -63,7 +63,10 @@ public:
 
     void UpdateProgress(size_t writeSize)
     {
-        SetUpdateProgress(static_cast<float>(writeSize) / pkgFileSize_ * proportion_);
+        if (pkgFileSize_ != 0) {
+            readOffset_ += size;
+            SetUpdateProgress(static_cast<float>(writeSize) / pkgFileSize_ * proportion_);
+        }
     }
 protected:
     std::string name_;
