d0108f7287606cf2d69df7e3b63d44e11978d622
lidanyang <lidanyang12@huawei.com>
2023-11-25 16:27:37 +0800

    add lang support for image
    Signed-off-by: lidanyang <lidanyang12@huawei.com>

--- services/ui/view/component/img_view_adapter.cpp
+++ services/ui/view/component/img_view_adapter.cpp
@@ -16,6 +16,7 @@
 #include <sstream>
 #include <unistd.h>
 #include "core/render_manager.h"
+#include "language/language_ui.h"
 #include "log/log.h"
 #include "page/view_proxy.h"
 #include "updater/updater_const.h"
@@ -85,6 +86,7 @@ ImgViewAdapter::ImgViewAdapter(const UxViewInfo &info)
     valid_ = true;
     this->SetAutoEnable(false);
     SetViewCommonInfo(info.commonInfo);
+    GetRealImgPath();
     LOG(INFO) << "dir:" << dir_ << ", imgCnt:" << imgCnt_ << ", interval:" << interval_;
     if (interval_ == 0) {
         this->SetSrc(dir_.c_str());
@@ -101,6 +103,30 @@ bool ImgViewAdapter::IsValid(const UxImageInfo &info)
         return IsValidForStaticImg(info);
     }
     return IsValidForAnimator(info);
+}
+
+void ImgViewAdapter::GetRealImgPath()
+{
+    using namespace Lang;
+    if (Fs::exists(dir_)) {
+        return;
+    }
+    const static std::unordered_map<Language, std::string> postFixMap {
+        {Language::CHINESE, "chn"},
+        {Language::CHINESE, "eng"},
+        {Language::SPANISH, "esp"},
+    };
+    auto iter = postFixMap.find(LanguageUI::GetInstance().GetCurLanguage());
+    if (iter == postFixMap.end()) {
+        return;
+    }
+    std::string newPath = dir_ + "_" + iter->second + ".png";
+    if (!Fs::exists(newPath)) {
+        LOG(WARNING) << "newPath not existed " << newPath;
+        return;
+    }
+    dir_ = newPath;
+    return;
 }
 
 bool ImgViewAdapter::IsValidForStaticImg(const UxImageInfo &info)
