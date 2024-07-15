88b16f1ced727b8e9d8c23b735dc9055e0bb4418
unknown <zhangchun39@huawei.com>
2024-07-15 14:15:35 +0800

    add sign on server
    Signed-off-by: unknown <zhangchun39@huawei.com>

--- utils/utils.cpp
+++ utils/utils.cpp
@@ -266,9 +266,12 @@ void DoShutdown()
     DoReboot("shutdown");
 }
 
-std::string GetCertName()
+std::string __attribute__((weak)) GetCertName()
 {
 #ifndef UPDATER_UT
+/*
+* if you use your own cert, youn need to replace it.
+*/
     static std::string signingCertName = "/etc/certificate/signing_cert.crt";
 #else
     static std::string signingCertName = "/data/updater/src/signing_cert.crt";
