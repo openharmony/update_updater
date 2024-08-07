39dee7ae4f7d615cc07a10d4f1107c1bdb1673a0
liaoxianyi <liaoxianyi@h-partners.com>
2024-08-07 06:59:46 +0000

    sd卡升级优化
    Signed-off-by: liaoxianyi <liaoxianyi@h-partners.com>

--- services/sdcard_update/sdcard_update.h
+++ services/sdcard_update/sdcard_update.h
@@ -29,6 +29,7 @@ extern "C" {
 #endif
 UpdaterStatus GetSdcardPkgsPath(UpdaterParams &upParams);
 UpdaterStatus GetSdcardPkgsFromDev(UpdaterParams &upParams);
+UpdaterStatus GetFactoryInternalPkgs(UpdaterParams &upParams);
 #ifdef __cplusplus
 #if __cplusplus
 }
