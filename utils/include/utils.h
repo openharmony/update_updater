a74e3c8f3490adb81d1664e2e1e8a2cc2053ac35
zhangcan42 <zhangcan42@h-partners.com>
2024-11-13 11:09:20 +0000

    add ConvertStrToNum function
    Signed-off-by: zhangcan42 <zhangcan42@h-partners.com>

--- utils/include/utils.h
+++ utils/include/utils.h
@@ -103,6 +103,7 @@ void TrimString (std::string &str);
 bool RestoreconPath(const std::string &path);
 std::string TrimUpdateMode(const std::string &mode);
 bool IsEsDevice();
+bool ConvertToLongLong(const std::string &str, int64_t &value);
 #ifndef __WIN32
 void SetFileAttributes(const std::string& file, uid_t owner, gid_t group, mode_t mode);
 #endif
