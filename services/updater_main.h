b9fbe098fa813de05297aefb17cf85db8b63c538
Rayllll <leizhonghang@huawei.com>
2025-02-08 10:29:34 +0000

    update services/updater_main.h.
    Signed-off-by: Rayllll <leizhonghang@huawei.com>

--- services/updater_main.h
+++ services/updater_main.h
@@ -70,7 +70,7 @@ int32_t VerifySpecialPkgs([[maybe_unused]]UpdaterParams &upParams);
 void UpdaterVerifyFailEntry(bool verifyret);
 bool IsSpareBoardBoot(void);
 bool IsNeedWipe();
-bool NotifySdUpdateReboot(const UpdaterParams &upParams = {});
+bool NotifySdUpdateReboot(const UpdaterParams &upParams);
 void NotifyReboot(const std::string& rebootTarget, const std::string &rebootReason, const std::string &extData = "");
 UpdaterStatus NotifyActionResult(UpdaterParams &upParams,
     UpdaterStatus &status, const std::vector<NotifyAction> &notifyActionVec);
