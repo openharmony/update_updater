4ca77f73cbb9c68b183f627433eceda93e1524be
zhangcan42 <zhangcan42@h-partners.com>
2025-03-27 09:27:31 +0000

    编译规范整改
    Signed-off-by: zhangcan42 <zhangcan42@h-partners.com>

--- test/unittest/script/threadpool_unittest.cpp
+++ test/unittest/script/threadpool_unittest.cpp
@@ -25,7 +25,7 @@
 #include "script_instruction.h"
 #include "script_manager.h"
 #include "script_utils.h"
-#include "thread_pool.h"
+#include "threadpool/thread_pool.h"
 #include "unittest_comm.h"
 
 using namespace std;
