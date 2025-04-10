ebabcc59770dbd978672a60751709e7e8e521392
陈凯 <chenkai172@huawei.com>
2025-04-10 02:55:43 +0000

    update services/ptable_parse/ptable.cpp.
    Signed-off-by: 陈凯 <chenkai172@huawei.com>

--- services/ptable_parse/ptable.cpp
+++ services/ptable_parse/ptable.cpp
@@ -560,13 +560,13 @@ bool Ptable::GetPartionInfoByName(const std::string &partitionName, PtnInfo &ptn
         LOG(INFO) << "find partition name " << partitionName;
         return true;
     }
-    int updateSlot;
     std::string partitionNameAB = partitionName;
-    if (Utils::IsUpdaterMode()) {
-        partitionNameAB += (GetCurrentSlot() == 1 ? PARTITION_A_SUFFIX : PARTITION_B_SUFFIX);
-    } else {
-        partitionNameAB += (GetCurrentSlot() == 1 ? PARTITION_B_SUFFIX : PARTITION_A_SUFFIX);
+    int updateSlot = Utils::GetUpdateSlot();
+    if (updateSlot < 1 || updateSlot > 2) { // 2 : slot b
+        LOG(ERROR) << "get update slot fail";
+        return false;
     }
+    partitionNameAB += (updateSlot == SLOT_A ? PARTITION_A_SUFFIX : PARTITION_B_SUFFIX);
     if (findPart(partitionNameAB)) {
         LOG(INFO) << "find partitionAB name " << partitionNameAB;
         return true;
