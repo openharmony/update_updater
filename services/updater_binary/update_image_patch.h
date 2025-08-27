355438b78f3aa9d0a9e6945010ae38cef4b91e65
陈凯 <chenkai172@huawei.com>
2025-08-27 01:46:24 +0000

    update services/updater_binary/update_image_patch.h.
    Signed-off-by: 陈凯 <chenkai172@huawei.com>

--- services/updater_binary/update_image_patch.h
+++ services/updater_binary/update_image_patch.h
@@ -60,8 +60,8 @@ public:
     USInstrImageShaCheck() {}
     virtual ~USInstrImageShaCheck() {}
     int32_t Execute(Uscript::UScriptEnv &env, Uscript::UScriptContext &context) override;
-    virtual GetParam(Uscript::UScriptContext &context, CheckPara &para);
-    virtual GetParam(Uscript::UScriptContext &context, CheckPara &para);
+    virtual std::string GetDevicePath(const std::string &partName);
+    virtual std::string GetHashData(const CheckPara &para, const size_t length);
 private:
     int32_t ExecuteShaCheck(Uscript::UScriptEnv &env, Uscript::UScriptContext &context);
     int32_t GetParam(Uscript::UScriptContext &context, CheckPara &para);
