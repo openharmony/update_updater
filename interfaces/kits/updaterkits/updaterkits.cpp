d35389b799d8cc2db399a7877e50a645bf35523d
刘勤亮 <liuqinliang1@h-partners.com>
2025-09-04 21:06:28 +0800

    add reboot func commit
    Signed-off-by: 刘勤亮 <liuqinliang1@h-partners.com>

--- interfaces/kits/updaterkits/updaterkits.cpp
+++ interfaces/kits/updaterkits/updaterkits.cpp
@@ -147,7 +147,7 @@ static bool WriteToMiscAndResultFileRebootToUpdater(const struct UpdateMessage &
     std::string writeMiscAfter = "0x80000008";
     WriteUpdaterResultFile(pkgPath, writeMiscAfter);
     if (rebootFunc != nullptr && rebootFunc() != 0) {
-        LOG(ERROR) << "Reboot Callback Failed";
+        LOG(ERROR) << "reboot func Failed";
         return false;
     } else if (rebootFunc == nullptr) {
         DoReboot("updater:reboot to updater to trigger update");
