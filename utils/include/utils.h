2f59bfa86d66e2abe16026a0e5d0e0b49c36da5c
chenkai11 <chenkai172@h-partners.com>
2026-01-10 10:15:24 +0800

    update: 更新文件 utils.h
    Signed-off-by: chenkai11 <chenkai172@h-partners.com>

--- utils/include/utils.h
+++ utils/include/utils.h
@@ -73,6 +73,7 @@ int32_t DeleteFile(const std::string& filename);
 std::vector<std::string> SplitString(const std::string &str, const std::string del = " \t");
 std::string Trim(const std::string &str);
 std::string ConvertSha256Hex(const uint8_t* shaDigest, size_t length);
+void UmountUserdata();
 void UpdaterDoReboot(const std::string& rebootTarget, const std::string &rebootReason,
     const std::string& extData = "");
 void DoShutdown(const std::string &shutdownReason);
