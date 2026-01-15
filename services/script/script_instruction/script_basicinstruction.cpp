598acdafa92ee4815f024a72607f708d488811fc
ck2001 <chaokai1@h-partners.com>
2026-01-15 15:18:15 +0800

    update: 更新文件 script_basicinstruction.cpp
    Signed-off-by: ck2001 <chaokai1@h-partners.com>

--- services/script/script_instruction/script_basicinstruction.cpp
+++ services/script/script_instruction/script_basicinstruction.cpp
@@ -199,6 +199,7 @@ int32_t UScriptInstructionDeleteDir::Execute(Uscript::UScriptEnv &env, Uscript::
         }
         std::string realPathStr(realPath);
         if (realPathStr.find("/data/updater") != 0) {
+            USCRIPT_LOGE("The realPath %s prefix is not /data/updater", realPath);
             free(realpath);
             continue;
         }
