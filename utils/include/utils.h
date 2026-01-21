734bba26696658ab2db972151da9438e43b60b0f
wang-jiadong11 <wangjiadong14@h-partners.com>
2026-01-21 10:41:16 +0800

    update: 更新文件 utils.h
    Signed-off-by: wang-jiadong11 <wangjiadong14@h-partners.com>

--- utils/include/utils.h
+++ utils/include/utils.h
@@ -153,7 +153,7 @@ int SetParameter(const char *key, const char *value);
 extern "C" {
 #endif
 #endif
-void InitLogger(const std::string &tag, const bool &isComaress);
+void InitLogger(const std::string &tag, const bool &isCompress);
 #ifdef __cplusplus
 #if __cplusplus
 }
