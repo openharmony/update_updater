d97edf598f2c07d89277233119ccca156858e87f
chenkai11 <chenkai172@h-partners.com>
2026-03-02 19:58:00 +0800

    update: 更新文件 utils_fs.h
    Signed-off-by: chenkai11 <chenkai172@h-partners.com>

--- utils/include/utils_fs.h
+++ utils/include/utils_fs.h
@@ -29,7 +29,7 @@ int64_t GetFilesFromDirectory(const std::string &path, std::vector<std::string> 
 bool RemoveDir(const std::string &path);
 bool IsFileExist(const std::string &path);
 bool IsDirExist(const std::string &path);
-bool IsMetadataEncrypt();
+bool ReadFileToString(int fd, std::string &content);
 } // Utils
 } // Updater
 #endif // UTILS_FS_H
