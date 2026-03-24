50ad7ee1a7884e9ddf7dacbd8ac4024d7fc976b5
curried <zhuxinyan@h-partners.com>
2026-03-24 11:19:12 +0800

    Dynamic log level support sensitive information
    Signed-off-by: curried <zhuxinyan@h-partners.com>

--- services/include/log/dynamic_log.h
+++ services/include/log/dynamic_log.h
@@ -22,13 +22,15 @@ namespace Updater {
 
 #define LOGDYN(format, ...) Updater::Logger(Updater::GetDynamicLogLevel(), (UPDATER_LOG_FILE_NAME), (__LINE__), \
     (format), ##__VA_ARGS__)
+#define LOGDYN_SEN(format, ...) Updater::LoggerSensitiveDynamic((UPDATER_LOG_FILE_NAME), (__LINE__), \
+    (format), ##__VA_ARGS__)
 
 void SetDynamicLogLevel(int level);
 int GetDynamicLogLevel();
 
 // 使用 RAII 模式管理日志级别，作用域结束时自动恢复原级别。支持静默模式，避免频繁切换时产生过多日志。
 class DynamicLoggerGuard final {
-DISALLOW_COPY_MOVE(DynamicLoggerGuard);
+    DISALLOW_COPY_MOVE(DynamicLoggerGuard);
 
 public:
     explicit DynamicLoggerGuard(int targetLevel);
@@ -40,6 +42,9 @@ private:
     int oldLevel_{INFO};
     bool silent_{false};
 };
+
+// for sensitive information
+void LoggerSensitiveDynamic(const char* fileName, int32_t line, const char* format, ...);
 } // Updater
 
 #endif /* UPDATE_DYNAMIC_LOG_H */
