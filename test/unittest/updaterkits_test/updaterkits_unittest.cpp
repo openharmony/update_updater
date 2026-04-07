f8206b92b0bdc6888c718ee25807392080432cd0
LeonShu <shuzhan10@huawei.com>
2026-04-07 16:50:23 +0800

    for updater kits UT
    Signed-off-by: LeonShu <shuzhan10@huawei.com>

--- test/unittest/updaterkits_test/updaterkits_unittest.cpp
+++ test/unittest/updaterkits_test/updaterkits_unittest.cpp
@@ -1,21 +1,23 @@
 /*
-* Copyright (c) 2021 Huawei Device Co., Ltd.
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #include "updaterkits_unittest.h"
 #include <iostream>
 #include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 #include "securec.h"
 #include "updaterkits/updaterkits.h"
 
@@ -25,6 +27,7 @@ using namespace std;
 
 namespace UpdaterUt {
 const std::string MISC_FILE = "/data/updater/misc_ut";
+const std::string TEST_PACKAGE_PATH = "/data/updater/test_package.zip";
 
 void UpdaterKitsUnitTest::SetUpTestCase(void)
 {
@@ -84,4 +87,287 @@ HWTEST_F(UpdaterKitsUnitTest, updater_kits_test02, TestSize.Level1)
     EXPECT_EQ(ret, true);
     unlink(MISC_FILE.c_str());
 }
-} // namespace updater_ut
+
+// 测试 EstimatedEraseTime 函数
+HWTEST_F(UpdaterKitsUnitTest, updater_kits_test03, TestSize.Level1)
+{
+    std::string eraseType = "secure_erase";
+    uint32_t ret = EstimatedEraseTime(eraseType);
+    EXPECT_GE(ret, 0);
+}
+
+HWTEST_F(UpdaterKitsUnitTest, updater_kits_test04, TestSize.Level1)
+{
+    std::string eraseType = "";
+    uint32_t ret = EstimatedEraseTime(eraseType);
+    EXPECT_GE(ret, 0);
+}
+
+// 测试 RebootAndSecureErase 函数
+HWTEST_F(UpdaterKitsUnitTest, updater_kits_test05, TestSize.Level1)
+{
+    std::string eraseType = "secure_erase";
+    auto fp = std::unique_ptr<FILE, decltype(&fclose)>(fopen(MISC_FILE.c_str(), "wb+"), fclose);
+    EXPECT_NE(fp, nullptr);
+    bool ret = RebootAndSecureErase(eraseType);
+    EXPECT_EQ(ret, true);
+    unlink(MISC_FILE.c_str());
+}
+
+HWTEST_F(UpdaterKitsUnitTest, updater_kits_test06, TestSize.Level1)
+{
+    std::string eraseType = "";
+    bool ret = RebootAndSecureErase(eraseType);
+    EXPECT_EQ(ret, true);
+}
+
+// 测试 OTA_INTRAL 升级模式
+HWTEST_F(UpdaterKitsUnitTest, updater_kits_test07, TestSize.Level1)
+{
+    int fd = open(TEST_PACKAGE_PATH.c_str(), O_CREAT | O_RDWR, 0644);
+    if (fd >= 0) {
+        close(fd);
+    }
+    
+    std::vector<std::string> packageName = {TEST_PACKAGE_PATH};
+    auto fp = std::unique_ptr<FILE, decltype(&fclose)>(fopen(MISC_FILE.c_str(), "wb+"), fclose);
+    EXPECT_NE(fp, nullptr);
+    int ret = RebootAndInstallUpgradePackage(MISC_FILE, packageName, UPGRADE_TYPE_OTA_INTRAL);
+    EXPECT_EQ(ret, 0);
+    unlink(MISC_FILE.c_str());
+    unlink(TEST_PACKAGE_PATH.c_str());
+}
+
+// 测试 SUBPKG_UPDATE 升级模式
+HWTEST_F(UpdaterKitsUnitTest, updater_kits_test08, TestSize.Level1)
+{
+    int fd = open(TEST_PACKAGE_PATH.c_str(), O_CREAT | O_RDWR, 0644);
+    if (fd >= 0) {
+        close(fd);
+    }
+    
+    std::vector<std::string> packageName = {TEST_PACKAGE_PATH};
+    auto fp = std::unique_ptr<FILE, decltype(&fclose)>(fopen(MISC_FILE.c_str(), "wb+"), fclose);
+    EXPECT_NE(fp, nullptr);
+    int ret = RebootAndInstallUpgradePackage(MISC_FILE, packageName, UPGRADE_TYPE_SUBPKG_UPDATE);
+    EXPECT_EQ(ret, 0);
+    unlink(MISC_FILE.c_str());
+    unlink(TEST_PACKAGE_PATH.c_str());
+}
+
+// 测试带回调函数的重启
+HWTEST_F(UpdaterKitsUnitTest, updater_kits_test09, TestSize.Level1)
+{
+    int fd = open(TEST_PACKAGE_PATH.c_str(), O_CREAT | O_RDWR, 0644);
+    if (fd >= 0) {
+        close(fd);
+    }
+    
+    std::vector<std::string> packageName = {TEST_PACKAGE_PATH};
+    RebootFunType rebootFunc = []() { return 0; };
+    auto fp = std::unique_ptr<FILE, decltype(&fclose)>(fopen(MISC_FILE.c_str(), "wb+"), fclose);
+    EXPECT_NE(fp, nullptr);
+    int ret = RebootAndInstallUpgradePackage(MISC_FILE, packageName, UPGRADE_TYPE_OTA, rebootFunc);
+    EXPECT_EQ(ret, 0);
+    unlink(MISC_FILE.c_str());
+    unlink(TEST_PACKAGE_PATH.c_str());
+}
+
+// 测试回调函数失败场景
+HWTEST_F(UpdaterKitsUnitTest, updater_kits_test10, TestSize.Level1)
+{
+    int fd = open(TEST_PACKAGE_PATH.c_str(), O_CREAT | O_RDWR, 0644);
+    if (fd >= 0) {
+        close(fd);
+    }
+    
+    std::vector<std::string> packageName = {TEST_PACKAGE_PATH};
+    RebootFunType rebootFunc = []() { return -1; };
+    auto fp = std::unique_ptr<FILE, decltype(&fclose)>(fopen(MISC_FILE.c_str(), "wb+"), fclose);
+    EXPECT_NE(fp, nullptr);
+    int ret = RebootAndInstallUpgradePackage(MISC_FILE, packageName, UPGRADE_TYPE_OTA, rebootFunc);
+    EXPECT_EQ(ret, 0);
+    unlink(MISC_FILE.c_str());
+    unlink(TEST_PACKAGE_PATH.c_str());
+}
+
+// 测试空包名OTA升级（应该返回错误）
+HWTEST_F(UpdaterKitsUnitTest, updater_kits_test11, TestSize.Level1)
+{
+    std::vector<std::string> packageName;
+    int ret = RebootAndInstallUpgradePackage(MISC_FILE, packageName, UPGRADE_TYPE_OTA);
+    EXPECT_EQ(ret, 1);  // 1: invalid input
+}
+
+// 测试空包名OTA_INTRAL升级（应该返回错误）
+HWTEST_F(UpdaterKitsUnitTest, updater_kits_test12, TestSize.Level1)
+{
+    std::vector<std::string> packageName;
+    int ret = RebootAndInstallUpgradePackage(MISC_FILE, packageName, UPGRADE_TYPE_OTA_INTRAL);
+    EXPECT_EQ(ret, 1);  // 1: invalid input
+}
+
+// 测试带 --force_update_action 参数
+HWTEST_F(UpdaterKitsUnitTest, updater_kits_test13, TestSize.Level1)
+{
+    int fd = open(TEST_PACKAGE_PATH.c_str(), O_CREAT | O_RDWR, 0644);
+    if (fd >= 0) {
+        close(fd);
+    }
+    
+    std::vector<std::string> packageName = {TEST_PACKAGE_PATH, "--force_update_action=poweroff"};
+    auto fp = std::unique_ptr<FILE, decltype(&fclose)>(fopen(MISC_FILE.c_str(), "wb+"), fclose);
+    EXPECT_NE(fp, nullptr);
+    int ret = RebootAndInstallUpgradePackage(MISC_FILE, packageName, UPGRADE_TYPE_OTA);
+    EXPECT_EQ(ret, 0);
+    unlink(MISC_FILE.c_str());
+    unlink(TEST_PACKAGE_PATH.c_str());
+}
+
+// 测试带 --night_update 参数
+HWTEST_F(UpdaterKitsUnitTest, updater_kits_test14, TestSize.Level1)
+{
+    int fd = open(TEST_PACKAGE_PATH.c_str(), O_CREAT | O_RDWR, 0644);
+    if (fd >= 0) {
+        close(fd);
+    }
+    
+    std::vector<std::string> packageName = {TEST_PACKAGE_PATH, "--night_update"};
+    auto fp = std::unique_ptr<FILE, decltype(&fclose)>(fopen(MISC_FILE.c_str(), "wb+"), fclose);
+    EXPECT_NE(fp, nullptr);
+    int ret = RebootAndInstallUpgradePackage(MISC_FILE, packageName, UPGRADE_TYPE_OTA);
+    EXPECT_EQ(ret, 0);
+    unlink(MISC_FILE.c_str());
+    unlink(TEST_PACKAGE_PATH.c_str());
+}
+
+// 测试带 --shrink_info 参数
+HWTEST_F(UpdaterKitsUnitTest, updater_kits_test15, TestSize.Level1)
+{
+    int fd = open(TEST_PACKAGE_PATH.c_str(), O_CREAT | O_RDWR, 0644);
+    if (fd >= 0) {
+        close(fd);
+    }
+    
+    std::vector<std::string> packageName = {TEST_PACKAGE_PATH, "--shrink_info=test"};
+    auto fp = std::unique_ptr<FILE, decltype(&fclose)>(fopen(MISC_FILE.c_str(), "wb+"), fclose);
+    EXPECT_NE(fp, nullptr);
+    int ret = RebootAndInstallUpgradePackage(MISC_FILE, packageName, UPGRADE_TYPE_OTA);
+    EXPECT_EQ(ret, 0);
+    unlink(MISC_FILE.c_str());
+    unlink(TEST_PACKAGE_PATH.c_str());
+}
+
+// 测试带 --virtual_shrink_info 参数
+HWTEST_F(UpdaterKitsUnitTest, updater_kits_test16, TestSize.Level1)
+{
+    int fd = open(TEST_PACKAGE_PATH.c_str(), O_CREAT | O_RDWR, 0644);
+    if (fd >= 0) {
+        close(fd);
+    }
+    
+    std::vector<std::string> packageName = {TEST_PACKAGE_PATH, "--virtual_shrink_info=test"};
+    auto fp = std::unique_ptr<FILE, decltype(&fclose)>(fopen(MISC_FILE.c_str(), "wb+"), fclose);
+    EXPECT_NE(fp, nullptr);
+    int ret = RebootAndInstallUpgradePackage(MISC_FILE, packageName, UPGRADE_TYPE_OTA);
+    EXPECT_EQ(ret, 0);
+    unlink(MISC_FILE.c_str());
+    unlink(TEST_PACKAGE_PATH.c_str());
+}
+
+// 测试带 --screen_state 参数
+HWTEST_F(UpdaterKitsUnitTest, updater_kits_test17, TestSize.Level1)
+{
+    int fd = open(TEST_PACKAGE_PATH.c_str(), O_CREAT | O_RDWR, 0644);
+    if (fd >= 0) {
+        close(fd);
+    }
+    
+    std::vector<std::string> packageName = {TEST_PACKAGE_PATH, "--screen_state=on"};
+    auto fp = std::unique_ptr<FILE, decltype(&fclose)>(fopen(MISC_FILE.c_str(), "wb+"), fclose);
+    EXPECT_NE(fp, nullptr);
+    int ret = RebootAndInstallUpgradePackage(MISC_FILE, packageName, UPGRADE_TYPE_OTA);
+    EXPECT_EQ(ret, 0);
+    unlink(MISC_FILE.c_str());
+    unlink(TEST_PACKAGE_PATH.c_str());
+}
+
+// 测试多个参数组合
+HWTEST_F(UpdaterKitsUnitTest, updater_kits_test18, TestSize.Level1)
+{
+    int fd = open(TEST_PACKAGE_PATH.c_str(), O_CREAT | O_RDWR, 0644);
+    if (fd >= 0) {
+        close(fd);
+    }
+    
+    std::vector<std::string> packageName = {
+        TEST_PACKAGE_PATH,
+        "--force_update_action=poweroff",
+        "--night_update",
+        "--shrink_info=test",
+        "--virtual_shrink_info=test"
+    };
+    auto fp = std::unique_ptr<FILE, decltype(&fclose)>(fopen(MISC_FILE.c_str(), "wb+"), fclose);
+    EXPECT_NE(fp, nullptr);
+    int ret = RebootAndInstallUpgradePackage(MISC_FILE, packageName, UPGRADE_TYPE_OTA);
+    EXPECT_EQ(ret, 0);
+    unlink(MISC_FILE.c_str());
+    unlink(TEST_PACKAGE_PATH.c_str());
+}
+
+// 测试工厂重置命令
+HWTEST_F(UpdaterKitsUnitTest, updater_kits_test19, TestSize.Level1)
+{
+    std::string cmd = "--factory_wipe_data";
+    auto fp = std::unique_ptr<FILE, decltype(&fclose)>(fopen(MISC_FILE.c_str(), "wb+"), fclose);
+    EXPECT_NE(fp, nullptr);
+    bool ret = RebootAndCleanUserData(MISC_FILE, cmd);
+    EXPECT_EQ(ret, true);
+    unlink(MISC_FILE.c_str());
+}
+
+// 测试菜单重置命令
+HWTEST_F(UpdaterKitsUnitTest, updater_kits_test20, TestSize.Level1)
+{
+    std::string cmd = "--menu_wipe_data";
+    auto fp = std::unique_ptr<FILE, decltype(&fclose)>(fopen(MISC_FILE.c_str(), "wb+"), fclose);
+    EXPECT_NE(fp, nullptr);
+    bool ret = RebootAndCleanUserData(MISC_FILE, cmd);
+    EXPECT_EQ(ret, true);
+    unlink(MISC_FILE.c_str());
+}
+
+// 测试空misc文件路径
+HWTEST_F(UpdaterKitsUnitTest, updater_kits_test21, TestSize.Level1)
+{
+    std::string cmd = "--user_wipe_data";
+    bool ret = RebootAndCleanUserData("", cmd);
+    EXPECT_EQ(ret, false);
+}
+
+// 测试多个包路径场景
+HWTEST_F(UpdaterKitsUnitTest, updater_kits_test22, TestSize.Level1)
+{
+    std::string pkg1 = "/data/updater/package1.zip";
+    std::string pkg2 = "/data/updater/package2.zip";
+    
+    int fd1 = open(pkg1.c_str(), O_CREAT | O_RDWR, 0644);
+    if (fd1 >= 0) {
+        close(fd1);
+    }
+    
+    int fd2 = open(pkg2.c_str(), O_CREAT | O_RDWR, 0644);
+    if (fd2 >= 0) {
+        close(fd2);
+    }
+    
+    std::vector<std::string> packageName = {pkg1, pkg2};
+    auto fp = std::unique_ptr<FILE, decltype(&fclose)>(fopen(MISC_FILE.c_str(), "wb+"), fclose);
+    EXPECT_NE(fp, nullptr);
+    int ret = RebootAndInstallUpgradePackage(MISC_FILE, packageName, UPGRADE_TYPE_OTA);
+    EXPECT_EQ(ret, 0);
+    unlink(MISC_FILE.c_str());
+    unlink(pkg1.c_str());
+    unlink(pkg2.c_str());
+}
+} // namespace UpdaterUt
