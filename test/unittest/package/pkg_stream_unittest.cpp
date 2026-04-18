b95783d8b4c162c0a399fc57d2a7528d4ae79fb1
liuqinliang <liuqinliang1@h-partners.com>
2026-04-18 19:36:35 +0800

    update: 更新文件 pkg_stream_unittest.cpp
    Signed-off-by: liuqinliang <liuqinliang1@h-partners.com>

--- test/unittest/package/pkg_stream_unittest.cpp
+++ test/unittest/package/pkg_stream_unittest.cpp
@@ -366,7 +366,7 @@ HWTEST_F(MemoryMapStreamTest, MemoryMapStreamReadWithInsufficientBuffer, TestSiz
  
 HWTEST_F(MemoryMapStreamTest, MemoryMapStreamReadSuccess, TestSize.Level1)
 {
-    strncpy_s(memBuffer_.buffer, sizeof(memBuffer_.buffer), "Test data for memory map", 25);
+    memcpy_s(memBuffer_.buffer, sizeof(memBuffer_.buffer), "Test data for memory map", 25);
     MemoryMapStream mms(nullptr, "test.bin", memBuffer_, PkgStream::PkgStreamType_MemoryMap);
     PkgBuffer data(100);
     data.data.resize(100);
@@ -722,7 +722,7 @@ HWTEST_F(FlowDataStreamTest, FlowDataStreamReadWithMemcpyFail, TestSize.Level1)
  
 HWTEST_F(MemoryMapStreamTest, MemoryMapStreamReadWithEmptyDataVector, TestSize.Level1)
 {
-    strncpy_s(memBuffer_.buffer, sizeof(memBuffer_.buffer), "Test data for memory map", 25);
+    memcpy_s(memBuffer_.buffer, sizeof(memBuffer_.buffer), "Test data for memory map", 25);
     MemoryMapStream mms(nullptr, "test.bin", memBuffer_, PkgStream::PkgStreamType_MemoryMap);
     PkgBuffer data;
     data.buffer = nullptr;
