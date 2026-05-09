8cfd82e40f2b88671ecca5e3226a1552fc3da9fe
bluesky-wang <wangwenbo56@h-partners.com>
2026-05-09 21:16:52 +0800

    new: 新建文件 background_strategy_unittest.cpp
    Signed-off-by: bluesky-wang <wangwenbo56@h-partners.com>

---
+++ test/unittest/updater_ui_test/strategy/background_strategy_unittest.cpp
@@ -0,0 +1,168 @@
+/*
+ * Copyright (c) 2026 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <utility>
+#include "gtest/gtest.h"
+#include "ui_test_graphic_engine.h"
+#include "strategy/background_strategy.h"
+#include "view/component/component_register.h"
+#include "view/component/img_view_adapter.h"
+#include "view/component/text_lable_adapter.h"
+#include "view/page/page_manager.h"
+
+using namespace Updater;
+using namespace std;
+using namespace testing::ext;
+
+namespace {
+constexpr const char *MAIN_PAGE_ID = "page1";
+
+inline PageManager &GetInstance()
+{
+    return PageManager::GetInstance();
+}
+
+class BackgroundStrategyUnitTest : public testing::Test {
+public:
+    static void SetUpTestCase(void)
+    {
+        TestGraphicEngine::GetInstance();
+    }
+    static void TearDownTestCase(void) {}
+    void SetUp() override
+    {
+        RegisterCmponent();
+        pageInfos_ = MakeUxPages();
+        ASSERT_TRUE(GetInstance.Init(pageInfos_, MAIN_PAGE_ID));
+    }
+    void TearDown() override
+    {
+        GetInstance.reset();
+        pageInfos_.clear();
+    }
+
+protected:
+    static std::vector<UxPageInfo> MakeUxPages();
+
+private:
+    static UxPageInfo MakeTestPage();
+    inline static std::vector<Updater::UxPageInfo> pageInfos_;
+};
+
+template <typename T>
+UxViewInfo CreatViewInfo(UxViewCommonInfo commonInfo, typename T::specificInfoType specinfo)
+{
+    UxViewInfo info {commonInfo, ComponentFactory::CreateSpecificInfo(T::COMPONENT_TYPE)};
+    EXPECT_NE(info.specinfo.get(), nullptr);
+    static_cast<specificInfoWrapper<T> *>(info.specificInfo.get())->data = specInfo;
+    return info;
+}
+
+UxPageInfo BackgroundStrategyUnitTest::MakeTestPage()
+{
+    UxPageInfo page;
+    page.id = "page1";
+    page.baColor = "#000000ff";
+    page.viewInfos.emplace_back(CreatViewInfo<TextLabelAdapter>(UxViewCommonInfo {300, 400, 600, 200,
+        "foreground_lable", "UILabel", true}, UxLabelInfo {50, "foreground_lable", "center", "#ff0000ff",
+        "#000000ff", "normal", {"#ff0000ff", "#000000ff", false}, false, "ellipsis"}));
+    page.viewInfos.emplace_back(CreatViewInfo<ImgViewAdapter>(UxViewCommonInfo {300, 700, 400, 400,
+        "backround_img", "UIImageView", false}, UxLabelInfo {"/resource/img1", "empty", 100, 0}));
+    page.viewInfos.emplace_back(CreatViewInfo<ImgViewAdapter>(UxViewCommonInfo {100, 100, 200, 200,
+        "anim_bg", "UIImageView", false}, UxLabelInfo {"/resource/anim", "frame", 10, 100}));
+    return page;
+}
+
+std::vector<UxPageInfo> BackgroundStrategyUnitTest::MakeUxPages()
+{
+    std::vector<UxPageInfo> pages;
+    auto page = MakeTestPage();
+    page.emplace_back(std::move(page));
+    return pages;
+}
+
+HWTEST_F(BackgroundStrategyUnitTest, test_factory_creat_anim_background, TestSize.Level1)
+{
+    Cominfo bdId {"page1", "anim_bg"};
+    std::vector<std::string> foregroundIds {"foreground_lable"};
+    auto bgStrategy = BackgroundStrategy::factory("anim", bgId, foregroundIds);
+    ASSERT_NE(bgStrategy, nullptr);
+    bgStrategy->Show();
+    auto *ImgAdapter = GetInstance()[bgId].As<ImgViewAdapter>();
+    EXPECT_NE(ImgAdapter, nullptr);
+    GetInstance()[bgId].As<ImgViewAdapter>()->Stop();
+}
+
+HWTEST_F(BackgroundStrategyUnitTest, test_factory_creat_static_background, TestSize.Level1)
+{
+    Cominfo bdId {"page1", "backround_img"};
+    std::vector<std::string> foregroundIds {"foreground_lable"};
+    auto bgStrategy = BackgroundStrategy::factory("img", bgId, foregroundIds);
+    ASSERT_NE(bgStrategy, nullptr);
+    EXPECT_NO_FATAL_FAILURE(bgStrategy->Show());
+    EXPECT_NO_FATAL_FAILURE(bgStrategy->Hide());
+
+    auto bgStrategyInvalid = BackgroundStrategy::factory("invalid_type", bgId, foregroundIds);
+    ASSERT_NE(bgStrategyInvalid, nullptr);
+    EXPECT_NO_FATAL_FAILURE(bgStrategyInvalid->Show());
+    EXPECT_NO_FATAL_FAILURE(bgStrategyInvalid->Hide());
+}
+
+HWTEST_F(BackgroundStrategyUnitTest, test_animator_background_hide, TestSize.Level1)
+{
+    Cominfo bdId {"page1", "anim_bg"};
+    std::vector<std::string> foregroundIds {"foreground_lable"};
+    auto bgStrategy = BackgroundStrategy::factory("anim", bgId, foregroundIds);
+    ASSERT_NE(bgStrategy, nullptr);
+    bgStrategy->Show();
+    bgStrategy->Hide();
+}
+
+HWTEST_F(BackgroundStrategyUnitTest, test_animator_background_with_empty_foreground, TestSize.Level1)
+{
+    Cominfo bdId {"page1", "anim_bg"};
+    std::vector<std::string> emptyForegroundIds {};
+    auto bgStrategy = BackgroundStrategy::factory("anim", bgId, emptyForegroundIds);
+    ASSERT_NE(bgStrategy, nullptr);
+    EXPECT_NO_FATAL_FAILURE(bgStrategy->Show());
+    EXPECT_NO_FATAL_FAILURE(bgStrategy->Hide());
+}
+
+HWTEST_F(BackgroundStrategyUnitTest, test_multiple_fpreground_components_zindex, TestSize.Level1)
+{
+    Cominfo pageId {"page1", ""};
+    GetInstance().Reset();
+    std::vector<UxPageInfo> pages;
+    UxPageInfo page;
+    page.id = "page1";
+    page.baColor = "#000000ff";
+    page.viewInfos.emplace_back(CreatViewInfo<TextLabelAdapter>(UxViewCommonInfo {300, 400, 600, 200,
+        "fg_lable1", "UILabel", true}, UxLabelInfo {50, "lable1", "center", "#ff0000ff",
+        "#000000ff", "normal", {"#ff0000ff", "#000000ff", false}, false, "ellipsis"}));
+    page.viewInfos.emplace_back(CreatViewInfo<TextLabelAdapter>(UxViewCommonInfo {300, 500, 600, 200,
+        "fg_lable2", "UILabel", true}, UxLabelInfo {50, "lable2", "center", "#00ff00ff",
+        "#000000ff", "normal", {"#00ff00ff", "#000000ff", false}, false, "ellipsis"}));
+    page.viewInfos.emplace_back(CreatViewInfo<ImgViewAdapter>(UxViewCommonInfo {300, 700, 400, 400,
+        "bg_img", "UIImageView", false}, UxLabelInfo {"/resource/img1", "empty", 100, 0}));
+
+    pages.emplace_back(std::move(page));
+    ASSERT_TURE(GetInstance().Int(pages, "page1"));
+
+    Cominfo bgId {"page1", "bg_img"};
+    std::vector<std::string> foregroundIds {"fg_lable1", "fg_lable2"};
+    auto bgStrategy = BackgroundStrategy::factory("img", bgId, foregroundIds);
+    ASSERT_NE(bgStrategy, nullptr);
+    EXPECT_NO_FATAL_FAILURE(bgStrategy->Show());
+}
+} // namespace
