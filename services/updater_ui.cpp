da32abc42c8162081c478861fee09a096fdef1ad
chenzihan4321 <chenzihan13@h-partners.com>
2026-05-14 21:29:10 +0800

    Signed-off-by: chenzihan4321 <chenzihan13@h-partners.com>
    fix:add

--- services/updater_ui.cpp
+++ services/updater_ui.cpp
@@ -154,7 +154,7 @@ DEFINE_ASYN_CALLBACK(OnLabelSDUpdateResEvt)
     }
     LOG(INFO) << "sdcard_intral_update write to misc success";
     if (auto res = UpdaterFromSdcard(upParams); res != UPDATE_SUCCESS) {
-        Utils::RemoveUpdateInfoFromMisc("sdcard_update");
+        Utils::RemoveUpdateInfoFromMisc("sdcard_intral_update");
         GetFacade().ShowLogRes(res == UPDATE_CORRUPT ? TR(LOGRES_VERIFY_FAILED) : TR(LOGRES_UPDATE_FAILED));
         GetFacade().ShowFailedPage();
         return;
