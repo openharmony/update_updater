2098eaa5595cc3714fc1f406528233391c2d0491
m0_63758985 <hansiyun@h-partners.com>
2026-06-15 19:29:49 +0800

    update: 更新文件 box_progress_adapter.h
    Signed-off-by: m0_63758985 <hansiyun@h-partners.com>

--- services/ui/view/component/box_progress_adapter.h
+++ services/ui/view/component/box_progress_adapter.h
@@ -42,7 +42,7 @@ public:
     explicit BoxProgressAdapter(const UxViewInfo &info);
     virtual ~BoxProgressAdapter() {}
     void SetValue(float value);
-    void SetVisible(bool isVisible);
+    void SetVisible(bool isVisible) override;
     void SetViewInfo(const UxViewInfo &info) override;
     [[nodiscard]] bool InitEp();
     static bool IsValid(const UxBoxProgressInfo &info);
