e4623563a401bb2c5e42cf2142d56184068dc78c
chenkai11 <chenkai172@h-partners.com>
2026-06-24 14:00:03 +0800

    update: 更新文件 utils.cpp
    Signed-off-by: chenkai11 <chenkai172@h-partners.com>

--- utils/utils.cpp
+++ utils/utils.cpp
@@ -362,8 +362,9 @@ void SyncFile(const std::string &dst)
         LOG(ERROR) << "open " << dst << " failed! err " << strerror(errno);
         return;
     }
+    fdsan_exchange_owner_tag(fd, 0, FDSAN_UPDATER_TAG);
     fsync(fd);
-    close(fd);
+    fdsan_close_with_tag(fd, FDSAN_UPDATER_TAG);
 }
 
 bool CopyFile(const std::string &src, const std::string &dest, bool isAppend)
@@ -405,12 +406,13 @@ bool CopyFileBySendFile(const std::string &srcFile, const std::string &destFile)
         LOG(ERROR) << srcFile << ", Open source file fail, errno: " << errno;
         return false;
     }
+    fdsan_exchange_owner_tag(source, 0, FDSAN_UPDATER_TAG);
 
     (void)memset_s(realPath, PATH_MAX + 1, 0, PATH_MAX + 1);
     std::string::size_type pos = destFile.find_last_of("/");
     if (pos == std::string::npos || realpath(destFile.substr(0, pos).c_str(), realPath) == nullptr) {
         LOG(ERROR) << destFile << " dest file dir realpath fail";
-        close(source);
+        fdsan_close_with_tag(source, FDSAN_UPDATER_TAG);
         return false;
     }
 
@@ -418,9 +420,10 @@ bool CopyFileBySendFile(const std::string &srcFile, const std::string &destFile)
     int32_t dest = open(destRealPath.c_str(), O_WRONLY | O_CREAT, 0644); // 0644 : file permission
     if (dest == -1) {
         LOG(ERROR) << destRealPath << ", Open dest file fail, errno: " << errno;
-        close(source);
+        fdsan_close_with_tag(source, FDSAN_UPDATER_TAG);
         return false;
     }
+    fdsan_exchange_owner_tag(dest, 0, FDSAN_UPDATER_TAG);
 
     struct stat fst{};
     bool result = false;
@@ -430,7 +433,8 @@ bool CopyFileBySendFile(const std::string &srcFile, const std::string &destFile)
             result = true;
         }
     }
-    close(source);
+    fdsan_close_with_tag(source, FDSAN_UPDATER_TAG);
+    fdsan_close_with_tag(dest, FDSAN_UPDATER_TAG);
     close(dest);
     return result;
 }
