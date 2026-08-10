6747e59b813ce43c981d4f0b2176f030638a18ff
rayllll <leizhonghang@huawei.com>
2026-08-10 11:50:56 +0800

    update: 更新文件 sdcard_update_action.h
    Signed-off-by: rayllll <leizhonghang@huawei.com>
    Signed-off-by: Rayllll <leizhonghang@huawei.com>

--- services/sdcard_update/action/sdcard_update_action.h
+++ services/sdcard_update/action/sdcard_update_action.h
@@ -76,7 +76,8 @@ public:
 class MountSdCardAction : public IMountAction {
 public:
     MountSdCardAction() : IMountAction() {}
-    MountSdCardAction(std::string item, std::string mountPoint) : IMountAction(), item_(item), mountPoint_(mountPoint) {}
+    MountSdCardAction(std::string item, std::string mountPoint)
+        : IMountAction(), item_(item), mountPoint_(mountPoint) {}
     ~MountSdCardAction() override {};
     UpdaterStatus Execute(UpdaterParams &upParams) override;
 private:
